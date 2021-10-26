1c3b73761520918f91dbb8ae1edf38dc508bfa52
Eessh <76767406+Eessh@users.noreply.github.com>
2021-10-26 19:14:23 +0530

    Update routes.py

--- Hackerramp/app/routes.py
+++ Hackerramp/app/routes.py
@@ -1,5 +1,18 @@
+from flask import render_template, request
+from werkzeug.utils import redirect
 from app import app
+
+bill = 0
 
 @app.route("/")
 def home():
-    return "Hehe working :)"
+    return render_template("home.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/billing", methods=["POST"])
+def handleBilling():
+    bill = request.form.values()
+    return redirect("/dashboard")
